f6ca77e04b6829431d02da69b7bb57d26cef5d7a
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2020-03-09 01:04:16 +0000

    repository

--- AirportVConc/src/mainProject/AirportVConc.java
+++ AirportVConc/src/mainProject/AirportVConc.java
@@ -1,4 +1,28 @@
 package mainProject;
 
+import sharedRegions.*;
+import entities.*;
+import commonInfra.*;
+import java.io.FileNotFoundException;
+import java.util.Date;
+
+/**
+ * Main thread of the program
+ */
 public class AirportVConc {
+
+    /**
+     * Repository log filename
+     */
+    public static final String filename = "log_" + new Date().toString().replace(' ', '_') + ".txt";
+
+    /**
+     * AirportVConc main's thread
+     * @param args unused main args
+     * @throws FileNotFoundException When file not found
+     */
+    public static void main(String args[]) throws FileNotFoundException {
+
+    }
+
 }
--- AirportVConc/src/sharedRegions/repositoryInfo.java
+++ AirportVConc/src/sharedRegions/repositoryInfo.java
@@ -1,4 +1,31 @@
 package sharedRegions;
 
+import mainProject.AirportVConc;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
 public class repositoryInfo {
+
+    private File f;
+    private PrintWriter pw;
+
+    /**
+     * Repository instantiation.
+     * @throws FileNotFoundException when there's no file
+     */
+    public repositoryInfo() throws FileNotFoundException {
+        f = new File(AirportVConc.filename);
+        pw = new PrintWriter(f);
+
+
+    }
+
+    /**
+     * Closes the printwriter when the simulation ends
+     */
+    public void closeWriter() {
+        pw.close();
+    }
 }
